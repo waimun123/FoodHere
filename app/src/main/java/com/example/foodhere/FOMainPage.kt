7b25151e5677d9cdcd01ac326fc77927df1a488e
waimun123 <51844621+waimun123@users.noreply.github.com>
2019-07-01 08:45:25 +0800

    Reworked Login and separated Food Outlet and Volunteer Register Page
    Stopping here for now until I wake up later because my dad needs to sleep now

---
+++ app/src/main/java/com/example/foodhere/FOMainPage.kt
@@ -0,0 +1,12 @@
+package com.example.foodhere
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FOMainPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_FOmain_page)
+    }
+}
